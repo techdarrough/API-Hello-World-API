db601b50edb63cc5782d3472a24e44c5f3439f68
Preston Darrough <techdarrough@gmail.com>
2022-03-27 12:45:17 -0700

    api

--- server.js
+++ server.js
@@ -13,10 +13,16 @@ mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopolo
 // MIDDLEWARE
 app.use(express.urlencoded({extended: true}))
 
+
 // ROUTES
 app.get('/', (req, res) => {
   res.send('Welcome to the Hello World! API')
 })
+
+
+// Languages: 
+const languagesController = require('./controllers/languages_controller.js')
+app.use('/languages', languagesController)
 
 // LISTEN
 app.listen(PORT, () => {
